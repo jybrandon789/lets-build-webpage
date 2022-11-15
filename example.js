8f1ecd25032d4b05909401501194e77e2c73728b
Brandon Na <brandonna213@gmail.com>
2022-11-15 18:51:48 -0500

    yes

---
+++ example.js
@@ -0,0 +1,7 @@
+alert('Hello World!');
+//Greet the user
+let message = "Hello User!";
+alert (message);
+let name = prompt("Actually, what is your name?");
+let newMessage = "Greetings, " + name;
+alert(newMessage);
---
+++ webpage.js
@@ -0,0 +1,5 @@
+alert('Hello!');
+//Greet the user
+let name = prompt("What is your name?");
+let newMessage = "Test your breaking bad knowledge, " + name;
+alert(newMessage);
